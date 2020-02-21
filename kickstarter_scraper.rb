af6933e8903c53f3c079316f0f6f12ce9663db23
James Rogers <jarogers095@gmail.com>
2020-02-21 06:55:17 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,7 @@ require "pry"
 def create_project_hash()
   html = File.read("fixtures/kickstarter.html")
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+
 end
 
 create_project_hash()
@@ -15,4 +15,5 @@ create_project_hash()
 # image link:   project.css("div.project-thumbnail a img").attribute("src").value
 # description:  project.css("p.bbcard_blurb").text
 # location      project.css("ul.project-meta li a span.location-name").text
-# percent       project.css("ul.project-stats li.first.funded strong").text
+# percent       project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+
